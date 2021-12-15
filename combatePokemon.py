4ec38c8d54ecceefbc2c265726e62743197b7f0c
Enric San Bartolomé Ferri <47865153+ensabar@users.noreply.github.com>
2021-12-15 14:37:21 +0100

    Empiezo el combate pokemon

---
+++ combatePokemon.py
@@ -0,0 +1,9 @@
+import random
+vida_pikachu = 80
+vida_squirtle = 90
+
+while vida_pikachu > 0 and vida_squirtle > 0:
+    #Se desenvuelven los turnos del combate
+
+    #Turno pikachu
+    ataque_pikachu = randint(1,2)
---
+++ test_while.py
@@ -0,0 +1,5 @@
+num = 12
+
+while num > 1:
+    print("El numero {} es mayor que 1." .format(num))
+    num -= 1
