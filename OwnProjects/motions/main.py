f466627c075c0a86ef16e07511a697fb0e7bbafc
Enric <ensabart@gmail.com>
2022-08-22 22:41:18 +0200

    JSON reading fixed

--- OwnProjects/motions/main.py
+++ OwnProjects/motions/main.py
@@ -5,17 +5,18 @@ from matplotlib.animation import FuncAnimation
 from matplotlib import animation
 
 type = input('Kind of motion to visualize?: ').lower()
-
+path = 'OwnProjects\motions\descriptions.json'
 
 if (type == 'linear'):
+    title, x_label, y_label = read_json_plot_properties(path, 'linear')
+
     a = float(input('Acceleration (m/s^2): '))
     vi = float(input('Initial velocity (m/s): ' ))
     duration = int(input('Time simulation (seg.): '))
     dataSet = linear_constant(a, vi, duration)
-    title, x_label, y_label = read_json_plot_properties('descriptions.json', 'Linear')
+    
     fig, ax = figure_settings(title, x_label, y_label)
 
-# create empty lists for the x and y data
 
 
 ani = animation.FuncAnimation(fig, update,  fargs=[dataSet], interval = 100)
--- OwnProjects/motions/reading.py
+++ OwnProjects/motions/reading.py
@@ -3,10 +3,10 @@ import json
 
 def read_json_plot_properties(file, plot_type):
     with open(file, 'r') as myfile:
-        data=myfile.read()
-
-    title = data[plot_type]['title']
-    x_label = data[plot_type]['x_label']
-    y_label = data[plot_type]['y_label']
-    data.close()
+        data = myfile.read()
+    obj = json.loads(data)
+    title = obj[plot_type]['title']
+    x_label = obj[plot_type]['x_label']
+    y_label = obj[plot_type]['y_label']
+    
     return title, x_label, y_label
