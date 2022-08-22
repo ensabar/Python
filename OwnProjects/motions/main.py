5ba98a3020f1a03b1bb87083bfbb7e12b2f8b059
Enric <ensabart@gmail.com>
2022-08-22 19:41:38 +0200

    Linear motion with animated plot

--- OwnProjects/motions/main.py
+++ OwnProjects/motions/main.py
@@ -1,10 +1,23 @@
-from OwnProjects.motions.motions import linear_constant
-from OwnProjects.motions.visualization import plot_function
-import visualization
-import motions
+from motions import *
+from visualization import *
+from matplotlib.animation import FuncAnimation
 
 a = 1
-t = range(1,10)
+duration = 100
+vi = 0
+type = input('Kind of motion to visualize?: ')
+type.lower()
 
-vf = linear_constant(a, t)
-plot_function(vf, vf)
+if (type == 'linear'):
+    a = float(input('Acceleration (m/s^2): '))
+    vi = float(input('Initial velocity (m/s): ' ))
+    duration = int(input('Time simulation (seg.): '))
+    #
+    dataSet = linear_constant(a, vi, duration)
+    
+
+# create empty lists for the x and y data
+
+
+ani = FuncAnimation(fig, update, frames= 500,  fargs=[dataSet])
+plt.show()
--- OwnProjects/motions/motions.py
+++ OwnProjects/motions/motions.py
@@ -1,3 +1,5 @@
+import numpy as np
+import matplotlib.pyplot as plt
 
 # Definition of different types of motion
 
@@ -6,7 +8,9 @@ def simple_harmonic(a, b):
 
     return a + b
 
-def linear_constant(a, t):
-    vi = 0
-    vf = vi +a*t
-    return vf
+def linear_constant(a, vi, duration):
+    duration = np.linspace(1,duration, 100)
+
+    vf = vi + a*duration
+    dataSet = np.array([duration, vf])
+    return dataSet
