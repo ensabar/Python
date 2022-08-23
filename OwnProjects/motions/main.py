f2b22eb8afd7be7a3d50600a591a7a801b97bd51
Enric <ensabart@gmail.com>
2022-08-23 23:17:55 +0200

    Revert "Modifiy subplot to figure"
    This reverts commit b35634443d9ab7d54d2ed35b1ca639c47a1858a0.

--- OwnProjects/motions/main.py
+++ OwnProjects/motions/main.py
@@ -1,28 +1,23 @@
 from motions import *
 from visualization import *
 from reading import *
-import numpy as np
-import matplotlib.pyplot as plt
-import matplotlib.pyplot as plt
-from matplotlib import animation
 from matplotlib.animation import FuncAnimation
-motion = input('Kind of motion to visualize?: ').lower()
+from matplotlib import animation
+
+type = input('Kind of motion to visualize?: ').lower()
 path = 'OwnProjects\motions\descriptions.json'
 
-if (motion == 'linear'):
+if (type == 'linear'):
     title, x_label, y_label = read_json_plot_properties(path, 'linear')
 
     a = float(input('Acceleration (m/s^2): '))
     vi = float(input('Initial velocity (m/s): ' ))
     duration = int(input('Time simulation (seg.): '))
     dataSet = linear_constant(a, vi, duration)
-    fig = figure_settings(title, x_label, y_label)
-    run_animation(fig, update, dataSet)
-elif (motion == 'accelerated'):  
-    plot_type = input ('Which kind of visualization you want? [real/phase]:  ')
-    if (plot_type == 'real'):
-        pass
     
-    elif (plot_type == 'phase'):
-        pass
+    fig, ax = figure_settings(title, x_label, y_label)
 
+
+
+ani = animation.FuncAnimation(fig, update,  fargs=[dataSet], interval = 100)
+plt.show()
--- OwnProjects/motions/motions.py
+++ OwnProjects/motions/motions.py
@@ -4,50 +4,12 @@ import matplotlib.pyplot as plt
 # Definition of different types of motion
 
 
-#def simple_harmonic(m, k):
-    # x = A*cos(wt - phi)
-    # v = -A*w * sin(wt - phi)
-    # A = |c1 + i*c2|
-    # phi = arctg(c2/c1)
-    # In the solution, c1 and c2 are two constants determined by the initial conditions 
-    # (specifically, the initial position at time t = 0 is c1, while the initial velocity is c2*ω),
-    # and the origin is set to be the equilibrium position.
-    # w = 2*pi*f
-    # f = 1 / T
-    # T = 2*pi*sqrt(m/k)
-k = 1.8 # N/m
-m = 3 # m
-c1 = np.float16(0) # initial position to zero
-c2 = np.float16(0) # initial velocity
-t = np.linspace(1,20,20)
-T = 2*np.pi*np.sqrt(m/k)
-w = 2*np.pi/T
+def simple_harmonic(a, b):
 
-xi = []
-x = []
-vi = []
-v = []
-
-for i in range(len(t)):
-    try:
-        phi = np.arctan(c2/c1)
-    except ZeroDivisionError:
-        phi = 0
-    
-    A = np.abs(c1 + c2*1j)
-
-    xi = A*np.cos(w*t[i] - phi)
-    x.append(xi)
-    c1 = np.int_(xi)
-    vi= -A*w * np.sin(w*t[i] - phi)
-    v.append(vi)
-    c2 = np.int_(vi)
-    #return a + b
+    return a + b
 
 def linear_constant(a, vi, duration):
     t = np.linspace(1,duration, duration)
     vf = vi + a*t
     dataSet = np.array([t, vf])
     return dataSet
-
-#def linear_accelerated()
