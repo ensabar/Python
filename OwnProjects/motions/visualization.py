b35634443d9ab7d54d2ed35b1ca639c47a1858a0
Enric <ensabart@gmail.com>
2022-08-23 23:17:33 +0200

    Modifiy subplot to figure

--- OwnProjects/motions/visualization.py
+++ OwnProjects/motions/visualization.py
@@ -1,30 +1,27 @@
-# animated_line_plot.py
-
 from random import randint
-
 import matplotlib.pyplot as plt
+from matplotlib import animation
 from matplotlib.animation import FuncAnimation
 
-
-
 def figure_settings(title, x_label, y_label):
-    fig, ax = plt.subplots()
-    
-    ax.set_title(title)
-    ax.set_xlabel(x_label)
-    ax.set_ylabel(y_label)
+    fig= plt.figure()
+    plt.title(title)
+    plt.xlabel(x_label)
+    plt.ylabel(y_label)
+    return fig
 
 
 # function that draws each frame of the animation
-def update(num, dataSet, ax):
-    # create the figure and axes objects
-    ax.clear()
-    ax.plot(dataSet[0, :num], dataSet[1, :num])
+def update(num, dataSet):
     
-    ax.set_xlim([0,max(dataSet[0]) + 0.1*max(dataSet[0])])
-    ax.set_ylim([0,max(dataSet[1]) + 0.1*max(dataSet[1])])
+    # create the figure and axes objects
+    plt.clf()
+    plt.plot(dataSet[0, :num], dataSet[1, :num])
+    
+    plt.xlim([0,max(dataSet[0]) + 0.1*max(dataSet[0])])
+    plt.ylim([0,max(dataSet[1]) + 0.1*max(dataSet[1])])
 
 # run the animation
-def run_animation(fig, animate):
-    ani = FuncAnimation(fig, animate, frames=20, interval=500, repeat=False)
+def run_animation(fig, animate, dataSet):
+    ani = animation.FuncAnimation(fig, update,  fargs=[dataSet], interval = 200, repeat_delay = 1000)
     plt.show()
