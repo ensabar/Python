c23ef16c8bcf29cdde1f40fab60d6d6a87b23838
Enric <ensabart@gmail.com>
2022-08-25 19:15:12 +0200

    harmonic modifications

--- OwnProjects/motions/harmonic.py
+++ OwnProjects/motions/harmonic.py
@@ -5,27 +5,33 @@ import matplotlib.pyplot as plt
 
 
 k = 1.8 # N/m
-m = 3 # g   
+m = 3 # g
 
-
-t = np.linspace(1,2*np.pi,50)
+rads = np.arange(0, 2*np.pi, 0.01)
 T = 2*np.pi*np.sqrt(m/k)
 w = 2*np.pi/T
 c1 = np.float16(0) # initial position to zero
 c2 = np.float16(5)*w # initial velocity
-
+t = np.linspace(0, 20, len(rads))
 xi = []
 x = []
 vi = []
 v = []
-for i in range(len(t)):
-    phi = np.divide(c2, c1, out=np.zeros_like(c2), where=c1!=0)
-    A = np.abs(c1 + c2*1j)
-    xi = A*np.cos(w*t[i] - phi)
-    x.append(xi)
-    c1 = np.float16(xi)
-    vi= -A*w * np.sin(w*t[i] - phi)
-    v.append(vi)
-    c2 = np.float16(vi)
-v = np.array(v)
-dataSet = np.array([t, x])
+
+phi = np.divide(c2, c1, out=np.zeros_like(c2), where=c1!=0) # to avoid 0/0
+A = np.abs(c1 + c2*1j)
+
+for rad in range(len(rads)):
+
+    
+    x.append(A*np.sin(w*t[rad] + phi))
+
+    #c1 = x[rad]
+    v.append(-A*w * np.sin(w*t[rad] - phi))
+
+    #c2 = v[rad]
+    
+plt.polar(v, x)
+plt.show()
+#v = np.array(v)
+#dataSet = np.array([rads, x])
--- OwnProjects/motions/linear.py
+++ OwnProjects/motions/linear.py
@@ -5,7 +5,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.pyplot as plt
 
-motion = input('Kind of motion to visualize?: ').lower()
+motion = 'linear'
 path = 'OwnProjects\motions\descriptions.json'
 plot_type = 'real'
 
@@ -14,7 +14,12 @@ title, x_label, y_label = read_json_plot_properties(path, motion, plot_type)
 a = float(input('Acceleration (m/s^2): '))
 vi = float(input('Initial velocity (m/s): ' ))
 duration = int(input('Time simulation (seg.): '))
+
 dataSet = linear_constant(a, vi, duration)
+
 fig = figure_settings(title, x_label, y_label)
+
 plt.xlim([0,max(dataSet[0]) + 0.1*max(dataSet[0])])
 plt.ylim([0,max(dataSet[1]) + 0.1*max(dataSet[1])]) 
+
+run_animation(fig, update, dataSet)
